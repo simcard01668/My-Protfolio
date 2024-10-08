7d8dcd12f2e5cbb724808e787c16d39717a96006
IsaacYam <howingyam0350@gmail.com>
2024-10-09 04:21:59 +0800

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallex</section>
+      <section>Service</section>
+      <section>Parallex</section>
+      <section>Portfolio</section>
+      <section>Protfolio</section>
+      <section>Protfolio</section>
+    </div>
+  )
+}
+
+export default App

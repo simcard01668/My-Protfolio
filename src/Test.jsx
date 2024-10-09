3533f345103a5d90862ee997b8effa0504a28c9d
IsaacYam <howingyam0350@gmail.com>
2024-10-10 04:11:10 +0800

    added test components

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,11 +1,34 @@
-import React from "react";
-import "./Test.scss";
-const Test = () => {
-  return import(
-    <div className="course">
-      <div className="box">Test</div>
-    </div>
-  );
-};
 
-export default Test;
+import React from 'react'
+import './Test.scss'
+import { delay, motion, stagger } from 'framer-motion'
+
+const Test = () => {
+  const listItem = ["item1", "item2", "item3", "item4", "item5", "item6", "item7", "item8", "item9", "item10"]
+
+  const variants = {
+    visible : (i)=>({ opacity: 1,
+      transition: {
+        delay: i * 0.3
+      },
+      x:100
+     }),
+     hidden : {
+      opacity: 0,
+      x:0
+     }
+
+  }
+  return (
+    <div className="course">
+      <motion.ul variants={variants} animate='visible' initial="hidden" >
+        {listItem.map((item, i)=>{
+          console.log(i)
+          return <motion.li variants={variants} key={item} custom={i}>{item}</motion.li>
+        })}
+      </motion.ul>
+    </div>
+  )
+}
+
+export default Test
